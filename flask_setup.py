494bfe253c5d45f6965e74ee5fa31f6f90f52173
crys <cryystalsong@gmail.com>
2021-01-12 22:45:54 -0800

    added python CORS and changed port

--- flask_setup.py
+++ flask_setup.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify
 #jsonify take data and automatically convert to JSON
 
+from flask_cors import CORS
+
 from scraper import main
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/search/<string:library>/<string:search_keywords>', methods=['GET'])
 def get_results(library,search_keywords):    
     results = main(library,search_keywords)
     return jsonify(results)
 
-app.run(host='0.0.0.0', port=6000)
+app.run(host='0.0.0.0', port=5000)
 
 
