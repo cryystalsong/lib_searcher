66fe34d70a1ef2af0d75331930b34ac55bef54ed
crys <cryystalsong@gmail.com>
2021-01-18 14:22:15 -0800

    modified scarper to use selenium, discarded bs4 scraper

--- scraper.py
+++ scraper.py
@@ -1,48 +1,12 @@
 ## IMPORTS
-from bs4 import BeautifulSoup as soup 
-from urllib.request import urlopen as uReq
-from PIL import Image
-
-bibliocommons_domains = {
-    "VPL": "vpl",
-    "NWPL": "newwestminster",
-    "RPL": "yourlibrary",
-    "BPL": "burnaby"
-}
-
-def generateBibliocommonsDomain(library_domain):
-    return "https://{}.bibliocommons.com".format(library_domain)
-
-def readURLtoSoup(url):
-    # opens the connection and downloads html page from url
-    uClient = uReq(url)
-
-    # parses html into a soup data structure to traverse html
-    # as if it were a json data type.
-    page_soup = soup(uClient.read(), "html.parser")
-    uClient.close()
-    return page_soup
-
-def getBookImg(url):
-    page_soup = readURLtoSoup(url)
-    img_src = page_soup.find("img",{"class": "jacketCover bib_detail"})['src']
-
-    try: 
-        uClient = uReq(img_src)    
-    except: 
-        img_src = "https:" + img_src
-        uClient = uReq(img_src)
-    finally:
-        im = Image.open(uClient)
-        uClient.close()
-        return img_src
-        # display(im)
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 # returns a list of result
-def parseEachContainer(containers, library, library_domain_url):
+def parseEachContainer(contents, library):
     results = []
 
-    for container in containers: 
+    for content in contents: 
         result = {
             "title": "",
             "author": "",
@@ -52,25 +16,18 @@ def parseEachContainer(containers, library, library_domain_url):
             "img_src": ""
         }
 
-        title = container.find("div",{"class": "jacket-cover-wrap hidden-md hidden-lg"}).a['title']
-        result["title"] = title
+        result["title"] = content.find_element_by_class_name('title-content').text
+        result["img_src"] = content.find_element_by_tag_name('img').get_attribute("src")
          
-        try: 
-            author = container.find("a",{"class": "author-link"}).text #sometimes unavailabile
-            result["author"] = author
+        try:             
+            result["author"] = content.find_element_by_class_name('author-link').text
         except:
-            author = None
+            result["author"] = None
 
-        book_link = container.find("div",{"class": "jacket-cover-wrap hidden-md hidden-lg"}).a["href"]
-        book_link = library_domain_url + book_link
-
-        availability = container.find("div",{"class": "manifestation-item-availability-block-wrap"}).span.text
-
+        result["book_link"] = content.find_element_by_class_name('cp-title').find_element_by_tag_name('a').get_attribute("href")
+        result["availability"] = content.find_element_by_class_name('manifestation-item-availability-block-wrap').find_element_by_tag_name('span').text
+        result["book_type_detail"] = content.find_element_by_class_name('format-info-main-content').text
         result["library_source"] = library
-        result["book_link"] = book_link
-        result["availability"] = availability
-        result["book_type_detail"] = container.find("div",{"class": "cp-format-info manifestation-item-format-info"}).contents[0].text
-        result["img_src"] = getBookImg(book_link) #TODO: fetch from lazy loading
 
         results.append(result)
 
@@ -86,21 +43,21 @@ def main(library, search_keywords):
         "RPL": "https://yourlibrary.bibliocommons.com/v2/search?query="+search_query+"&searchType=keyword",
         "BPL": "https://burnaby.bibliocommons.com/v2/search?query="+search_query+"&searchType=smart"
     }    
-        
-    page_soup = readURLtoSoup(library_search_urls[library])
+    
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(library_search_urls[library])
 
     response_object = {}
 
     response_object["library"] = library
 
-    containers = page_soup.findAll("div", {"class": "cp-search-result-item-content"})
-    response_object["result_count"] = len(containers)  
+    contents = driver.find_element_by_class_name('results').find_elements_by_tag_name('li')
+    response_object["result_count"] = len(contents)  
     response_object["results"] = []  
 
     if response_object["result_count"] <= 0:
         return response_object
     
-    library_domain_url = generateBibliocommonsDomain(bibliocommons_domains[library])  
-    response_object["results"] = parseEachContainer(containers, library, library_domain_url)
+    response_object["results"] = parseEachContainer(contents, library)
 
     return response_object
